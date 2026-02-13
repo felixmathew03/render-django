66687bfb20ccfa3ad5ffb2d10ff6862a2f83327c
felixmathew03 <felixmathewtt@gmail.com>
2026-02-13 15:38:58 +0530

    css added

--- core/urls.py
+++ core/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('tasks.urls')),
     path('login/',auth_views.LoginView.as_view(template_name='tasks/login.html'), name='login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name='tasks/login.html'), name='logout')
+    path('logout/',auth_views.LogoutView.as_view(next_page='login'), name='logout')
 ]
